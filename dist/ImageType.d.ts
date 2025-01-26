cf5e364dbc78170824f372845c5312d627a2540a
roshiroku <shir.oraan@gmail.com>
2025-01-26 17:25:11 +0200

    remove dist

--- dist/ImageType.d.ts
+++
@@ -1,8 +0,0 @@
-declare enum ImageType {
-    None = "",
-    JPG = "image/jpg",
-    JPEG = "image/jpeg",
-    PNG = "image/png",
-    WEBP = "image/webp"
-}
-export default ImageType;
--- dist/utils.d.ts
+++
@@ -1,4 +0,0 @@
-import ImageType from './ImageType';
-export declare const compareImages: (data1: ImageData, data2: ImageData) => Promise<number>;
-export declare const estimateFileSize: (src: string) => number;
-export declare const inferImageType: (src: string) => ImageType;
